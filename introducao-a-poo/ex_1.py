e1bab6fe271f6bf3a5ad0bb00b6a917b3f8840e9
Carolina Vasconcelos <carolinavpinheiro@gmail.com>
2021-12-07 19:06:13 -0300

    Feat: ex_1

--- introducao-a-poo/ex_1.py
+++ introducao-a-poo/ex_1.py
@@ -1,2 +1,7 @@
 # Em um contexto de orientação a objetos,
 # como podemos definir o que são mensagens e qual a sua importância?
+
+
+# Enviamos uma mensagem quando executamos um método de um objeto.
+# Sendo assim, toda ação de um objeto está associada ao envio de mensagens,
+# que está intimamente ligada a comunicação entre os objetos de uma aplicação.
