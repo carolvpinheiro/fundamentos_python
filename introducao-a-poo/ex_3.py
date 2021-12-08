1470706ac1aa5ad3beedd390e18634ca26ba0537
Carolina Vasconcelos <carolinavpinheiro@gmail.com>
2021-12-08 08:19:43 -0300

    Feat: ex_3

---
+++ introducao-a-poo/ex_3.py
@@ -0,0 +1,18 @@
+# E como poderíamos definir um círculo? 
+# Qual seu nome, atributos e comportamentos?
+
+import math
+
+PI = 3.14159
+
+
+class circulo:
+
+    def __init__(self, circunferencia):
+        self.circunferencia = circunferencia
+
+    def calcular_raio(self):
+        return self.circunferencia / (2 * PI)
+
+    def calcular_area(self):
+        print(PI * math.pow(self.calcular_raio, 2))
